f0afc6140332879de88179833ea23d6d9f07bb7c
Daniel Maes <dmaesj@gmail.com>
2020-05-29 23:04:07 -0700

    updated header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
 function App() {
   return (
@@ -12,16 +11,15 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className='App title'>Fib Calculator v2</h1>
+          <h1 className="App title">Fib Calculator Kubernetes</h1>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">OtherPage</Link> 
+          <Link to="/otherpage">OtherPage</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
         </div>
       </div>
-
     </Router>
   );
 }
